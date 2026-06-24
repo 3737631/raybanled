7760fa3706d5013514319252cc47ccfb76209f92
Paquito <paquito@example.com>
2026-06-24 04:31:39 +0200

    fix: update base path and add .nojekyll to deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/ventaelcapricho-/',
+    base: '/ventaelcapricho/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
